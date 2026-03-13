1fbbe81fa58cec306e9442a58c4afeb6f9da0d94
abeamonteiro <abeatrizbmonteiro@gmail.com>
2026-03-12 21:33:45 -0300

    ajustes no service

--- src/categorias/services/categorias.service.ts
+++ src/categorias/services/categorias.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Categorias } from '../entities/categorias.entity';
